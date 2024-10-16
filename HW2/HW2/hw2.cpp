66646eab4bf7f4613b90ba326d48f631e9c53595
toproof <41888060+toproof25@users.noreply.github.com>
2024-10-16 12:39:01 +0900

    과제2 수정

--- HW2/HW2/hw2.cpp
+++ HW2/HW2/hw2.cpp
@@ -6,19 +6,52 @@
 
 static int Day = 0, Time = 0;
 
+GLdouble eyex = 0, eyey = 0, eyez = 0;
+GLdouble centerx = 0, centery = 0, centerz = -1;
+GLdouble upx = 0, upy = 1, upz = 0;
+
+// 목표 시점을 저장할 변수
+GLdouble target_eyex = 0, target_eyey = 0, target_eyez = 0;
+GLdouble target_centerx = 0, target_centery = 0, target_centerz = -1;
+GLdouble target_upx = 0, target_upy = 1, target_upz = 0;
+
+bool isAnimating = false;
+
+// 변화량을 정의 (시점 변화 속도 조절)
+const GLdouble step = 0.01;
 
 void MyDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // 태양계 위에서 보기 추가
-    gluLookAt(
-        0, 0, 0.1,
-        0.0, -100.0, 0.0,
-        0.0, 1.0, .0
-    );
+    // 애니메이션 중이라면 점진적으로 카메라 시점을 변경
+    if (isAnimating) {
+        eyex += (target_eyex - eyex) * step;
+        eyey += (target_eyey - eyey) * step;
+        eyez += (target_eyez - eyez) * step;
 
+        centerx += (target_centerx - centerx) * step;
+        centery += (target_centery - centery) * step;
+        centerz += (target_centerz - centerz) * step;
+
+        upx += (target_upx - upx) * step;
+        upy += (target_upy - upy) * step;
+        upz += (target_upz - upz) * step;
+
+        // 카메라 이동이 거의 완료되면 애니메이션 중지
+        if (abs(target_eyex - eyex) < 0.001 &&
+            abs(target_eyey - eyey) < 0.001 &&
+            abs(target_eyez - eyez) < 0.001) {
+            isAnimating = false;
+        }
+    }
+
+    gluLookAt(
+        eyex, eyey, eyez,
+        centerx, centery, centerz,
+        upx, upy, upz
+    );
 
     glColor3f(1.0, 0.3, 0.3);
     glutWireSphere(0.2, 20, 16);
@@ -53,10 +86,32 @@ void MyKeyboard(unsigned char key, int x, int y) {
         glutPostRedisplay();
         break;
 
+    case 'q':
+        // 즉시 시점 변경
+        target_eyex = 0; target_eyey = 0; target_eyez = 0;
+        target_centerx = 0; target_centery = 0; target_centerz = -1;
+        target_upx = 0; target_upy = 1; target_upz = 0;
+        isAnimating = true;
+        glutPostRedisplay();
+        break;
+
+    case 'w':
+        // 목표 시점을 설정 (천천히 변경)
+        target_eyex = 0; target_eyey = 0; target_eyez = 0.1;
+        target_centerx = 0; target_centery = -1; target_centerz = 0;
+        target_upx = 0; target_upy = 1; target_upz = 0;
+        isAnimating = true;
+        glutPostRedisplay();
+        break;
+
     default:
         break;
     }
+}
 
+void Timer(int value) {
+    glutPostRedisplay();  // 디스플레이 함수 호출
+    glutTimerFunc(16, Timer, 0);  // 약 60FPS로 타이머 호출
 }
 
 int main(int argc, char** argv) {
@@ -71,6 +126,7 @@ int main(int argc, char** argv) {
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     glutDisplayFunc(MyDisplay);
     glutKeyboardFunc(MyKeyboard);
+    glutTimerFunc(16, Timer, 0);  // 타이머 함수 등록
     glutMainLoop();
     return 0;
 }
